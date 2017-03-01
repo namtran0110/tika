2fddd62dba1a70c89d17da8564678c09053e4100
Nam Tran <namtran0110@gmail.com>
2017-03-01 17:50:57 +0700

    ongoing: event_create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,29 @@ class EventsController < ApplicationController
     end
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :starts_at, :ends_at, :venue_id, :hero_image_url, :extended_html_description, :category_id)
   end
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
   def header_background_image_url
-    "http://a2.files.magneticmag.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTMzMDU3OTA1MDk5Mjg3MTcx.jpg"
+    "http://www.thatdrop.com/wp-content/uploads/2015/10/dimitri-vegas-like-mike2.jpg"
   end
 end
